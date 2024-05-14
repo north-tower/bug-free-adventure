950e34d4ae5132a854e148eed0fe1734f98948bb
north-tower <muneshmyke@gmail.com>
2024-05-14 18:25:20 +0300

    lk

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Aside from "@/components/Aside"
+import Header from '@/components/Header';
+import Dash from '@/components/Dash';
+
+export default function Home() {
+  return (
+    <>
+     
+    
+      <Dash />
+      
+    </>
+  );
+}
